17340eadc81c895badb7985f08dc47896d052930
rdayok <r.dayok@native.semicolon.africa>
2023-06-08 17:43:07 +0100

    updating my first javascript code

--- introduction/variable.js
+++ introduction/variable.js
@@ -3,7 +3,42 @@ let age = 23;
 const SCHOOL = "Semicolon Africa";
 number = 35;
 age = 21;
-SCHOOL = "Semicolon";
+// SCHOOL = "Semicolon";
 console.log(age);
 console.log(number);
 
+let var1 = 2;
+let var2 = 4;
+let var3 = 6;
+let variableOneHolder = var1;
+var1 = var2;
+var2 = var3;
+var3 = variableOneHolder;
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(true || false)
+console.log(true || !true)
+console.log(!false)
+var condition = 2 + 2 == 4;
+condition ? console.log("True") : console.log("False")
+
+console.log(2 + 4 * 3 % 5 - 1 * 6 /(1 + 1))
+
+let variableOne = 3;
+let variavleTwo = 4;
+let a = 4
+let x = a + 10;
+let y = a - 10
+let z = 6;
+
+let resultOne = 2 * ( variableOne + variavleTwo);
+
+let resultTwo = (variableOne % 10) + variableOne;
+
+let resultThree = variableOne ** variavleTwo;
+console.log(resultThree)
+
+
+let resultFour = a / ((a + 10) * (a - 10) * (a % 10));
