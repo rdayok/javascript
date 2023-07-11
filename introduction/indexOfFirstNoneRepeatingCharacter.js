544ed0495e4fd3f02ae2cf6ef9f03faa4ba3dcce
rdayok <r.dayok@native.semicolon.africa>
2023-07-11 11:58:28 +0100

    refactored the indexOf none repeating character in a word

--- introduction/indexOfFirstNoneRepeatingCharacter.js
+++ introduction/indexOfFirstNoneRepeatingCharacter.js
@@ -5,13 +5,10 @@ let word = prompt("Please enter a word: ");
 
 function indexOfFirstNoneRepeatingCharacter(word){
     for(let index = 0; index < word.length; index++){
-        let countOfRepeatition = 0;
         for(let index2 = 0; index2 < word.length; index2++){
-            if(word[index] == word[index2] && index != index2)countOfRepeatition++;
+            if(word[index] == word[index2] && index != index2) break;
+            if(index2 === word.length - 1) return index;
         }
-        if(countOfRepeatition == 0){
-            return index;
-        };
     }
     return -1;
 }
